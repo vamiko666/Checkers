ab6842bc70db54788b4877da78334b841a1e8f21
vamiko <drakon4ikstream@gmail.com>
2022-12-23 15:16:43 +0300

    no error yey

--- src/main/Player.java
+++ src/main/Player.java
@@ -26,40 +26,45 @@ public class Player {
     public void setIsWhite(boolean isWhite) {
         this.isWhite = isWhite;
     }
-    public void move(int x1,int y1,int x2,int y2, Field field) {
+
+    public void move(int x1, int y1, int x2, int y2, Field field) {
         Figure[][] board = field.getBoard();
-        if(board[x1][y1] !=null ){
+        if (board[x1][y1] != null) {
+            if(canMove(x1,y1,x2,y2, board)){
+
+            }
 
 
         }
     }
 
     public boolean canMove(int x1, int y1, int x2, int y2, Figure[][] board) {
-        Figure figure =board[x1][x2];
-        if(x2>9 || y2>9){
+        Figure figure = board[x1][y1];
+        if (x2 > 9 || y2 > 9) {
             return false;
         }
         if (figure.isWhite() == isWhite) {
-            if(figure.isWhite()==true){
-                if((board[x1+1][y1+1]==null || board[x1-1][y2+1]==null) & (x1+1==x2 || x1-1==x2) & y1==y2 ){
+            if (figure.isWhite()) {
+                //движение обычных
+                if ((board[x1 + 1][y1 + 1] == null || board[x1 - 1][y2 + 1] == null) & (x1 + 1 == x2 || x1 - 1 == x2) & y1 == y2) {
                     return true;
-                }else if(board[x2][y2]==null & (x1+2==x2 || x1-2==x2) & y1+2==y2){
+                } else if (board[x2][y2] == null & (x1 + 2 == x2 || x1 - 2 == x2) & y1 + 2 == y2) {
                     return true;
-
-                }else if(figure.isKing()){
-                    if(x1<x2) {
+                //if is a king
+                } else if (figure.isKing()) {
+                    if (x1 < x2) {
                         for (int i = x1; i <= x2; i++) {
                             for (int j = y1; j <= y2; j++) {
                                 int count = 0;
                                 if (board[i][j] != null)
                                     count += 1;
-                                if (count > 1) {
+                                if (count >1) {
                                     break;
                                 }
 
                             }
                         }
-                    }else{
+                    } else {
                         for (int i = x1; i >= x2; i--) {
                             for (int j = y1; j <= y2; j++) {
                                 int count = 0;
@@ -68,15 +73,17 @@ public class Player {
                                 if (count > 1) {
                                     break;
 
+                                }
+                            }
+
+                        }
+
                     }
                 }
-
             }
 
         }
+        return false;
     }
 }
-
-
-
 
